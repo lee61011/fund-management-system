01740011efae5430ed481fa5d58c35b657217244
lee61011 <lee61011@126.com>
2019-11-02 23:20:24 +0800

    连接 MongoDB 数据库

--- node-demo/node-app/server.js
+++ node-demo/node-app/server.js
@@ -1,5 +1,11 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+const db = require("./config/keys").mongoURI;   //  DB config
+mongoose.connect(db)
+    .then(() => console.log("Mongodb Connected"))
+    .catch(err => console.log(err))
 
 app.get("/",(req,res) => {
     res.send("Hello World!")
