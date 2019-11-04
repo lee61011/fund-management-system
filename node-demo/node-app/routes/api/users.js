085cacdc5ed344d546a04ce7fe34b8dea76b5c3d
lee61011 <lee61011@126.com>
2019-11-04 22:45:40 +0800

    搭建注册接口并存储数据

--- node-demo/node-app/routes/api/users.js
+++ node-demo/node-app/routes/api/users.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../../models/User");
 
 router.get("/test", (req, res) => {
     res.json({msg: "login works"})
+})
+
+router.post("/register", (req, res) => {
+    //  console.log(req.body);
+    //  查询数据库中是否拥有邮箱
+    User.findOne({email: req.body.email})
+        .then((user) => {
+            if (user) {
+                return res.status(400).json({email: "邮箱已被注册!"});
+            } else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar,
+                    password: req.body.password
+                })
+
+                //  密码加密
+                bcrypt.genSalt(10, function (err, salt) {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user => res.json(user))
+                            .catch(err => console.log(err));
+                    })
+                })
+            }
+        })
 })
 
 module.exports = router;
--- node-demo/node-app/server.js
+++ node-demo/node-app/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 
 const users = require("./routes/api/users");
 
 app.use("/api/users", users);
+//  使用 body-parser 中间件
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const db = require("./config/keys").mongoURI;   //  DB config
 mongoose.connect(db)
