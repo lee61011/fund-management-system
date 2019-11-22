f0bb61cd6139de32a2f4611e56a32b5e52d09a84
lee61011 <lee61011@126.com>
2019-11-22 23:59:04 +0800

    设置Register和404组件

---
+++ node-demo/node-vue-ele-app/client/src/views/404.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="notfound">
+        <img src="../assets.gif" alt="">
+    </div>
+</template>
+
+<style scope>
+.notfound {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+.notfound img {
+    width: 100%;
+    height: 100%;
+}
+</style>>
