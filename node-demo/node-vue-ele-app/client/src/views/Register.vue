f484fdb3f1771c8ae6d1e877dfb91d29b17229f0
lee61011 <lee61011@126.com>
2019-11-23 23:48:20 +0800

    element注册表单

--- node-demo/node-vue-ele-app/client/src/views/Register.vue
+++ node-demo/node-vue-ele-app/client/src/views/Register.vue
@@ -3,6 +3,31 @@
         <section class="form_container">
             <div class="manage_tip">
                 <span class="title">米修在线后台管理系统</span>
+                <el-form :model="registerUser" :rules="rules" ref="registerForm" label-width="80px" class="registerForm">
+                    <el-form-item label="用户名" prop="name">
+                        <el-input v-model="registerUser.name" placeholder="请输入用户名"></el-input>
+                    </el-form-item>
+                    <el-form-item label="邮箱" prop="email">
+                        <el-input v-model="registerUser.email" placeholder="请输入email"></el-input>
+                    </el-form-item>
+                    <el-form-item label="密码" prop="password">
+                        <el-input type="password" v-model="registerUser.password" placeholder="请输入密码"></el-input>
+                    </el-form-item>
+                    <el-form-item label="确认密码" prop="password2">
+                        <el-input type="password" v-model="registerUser.password2" placeholder="请确认密码"></el-input>
+                    </el-form-item>
+
+                    <el-form-item label="选择身份">
+                        <el-select v-model="registerUser.identity" placeholder="请选择身份">
+                            <el-option label="管理员" value="manager"></el-option>
+                            <el-option label="员工" value="employee"></el-option>
+                        </el-select>
+                    </el-form-item>
+
+                    <el-form-item>
+                        <el-button type="primary" class="submit_btn" @click="submitForm('registerForm')">注册</el-button>
+                    </el-form-item>
+                </el-form>
             </div>
         </section>
     </div>
@@ -11,7 +36,18 @@
 <script>
 export default {
     name: "register",
-    components: {}
+    components: {},
+    data(){
+        return {
+            registerUser: {
+                name: '',
+                email: '',
+                password: '',
+                password2: '',
+                identity: ''
+            }
+        }
+    }
 }
 </script>
 
@@ -38,5 +74,16 @@ export default {
     font-weight: bold;
     font-size: 26px;
     color: #fff;
+}
+
+.registerForm {
+    margin-top: 20px;
+    background-color: #fff;
+    padding: 20px 40px 20px 20px;
+    border-radius: 5px;
+    box-shadow: 0px 5px 10px #cccccc;
+}
+.submit_btn {
+    width: 100%;
 }
 </style>
