397397041318a1ef0dddbbd88c9a904adeae6b77
lee61011 <lee61011@126.com>
2019-12-01 23:49:25 +0800

    路由守卫和token过期处理

--- node-demo/node-vue-ele-app/client/src/http.js
+++ node-demo/node-vue-ele-app/client/src/http.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { Message, Loading } from 'element-ui';
+import router from './router';
 
 let loading;
 function startLoading() {
@@ -18,6 +19,11 @@ function endLoading() {
 axios.interceptors.request.use(config => {
     //  加载动画
     startLoading();
+
+    if (localStorage.eleToken) {
+        //  设置统一的请求头 header
+        config.headers.Authorization = localStorage.eleToken;
+    }
     return config;
 }, error => {
     return Promise.reject(error);
@@ -32,6 +38,17 @@ axios.interceptors.response.use(response => {
     //  错误提醒
     endLoading();
     Message.error(error.response.data);
+
+    //  获取错误状态码
+    const { status } = error.response;
+    if (status == 401) {
+        Message.error('token失效, 请重新登录!')
+        //  清除 token
+        localStorage.removeItem('eleToken');
+        //  跳转到登录页面
+        router.push('/login');
+    }
+
     return Promise.reject(error);
 })
 
--- node-demo/node-vue-ele-app/client/src/router/index.js
+++ node-demo/node-vue-ele-app/client/src/router/index.js
@@ -40,4 +40,14 @@ const router = new VueRouter({
   routes
 })
 
+//  路由守卫
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.eleToken ? true : false;
+  if (to.path == '/login' || to.path == '/register') {
+    next();
+  } else {
+    isLogin ? next() : next('/login')
+  }
+})
+
 export default router
